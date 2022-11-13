33ca2f9266eea6aa4f8fdbc021f2d6a11f23301b
CrisRmrz <cris_23-jun-2001@hotmail.com>
2022-11-13 15:18:10 -0600

    googlesignin terminado

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,9 @@
-const { response } = require('express');
+const { response, json } = require('express');
 const Usuario = require('../models/usuario');
 const bcryptjs = require('bcryptjs');
 const { generarJWT } = require('../helpers/generarJWT');
+const { googleVerify } = require('../helpers/google-verify');
+const { DefaultTransporter } = require('google-auth-library');
 
 const login = async (req, res = response) => {
 
@@ -11,29 +13,29 @@ const login = async (req, res = response) => {
 
         // Verificar si el email existe
         const usuario = await Usuario.findOne({ correo }); //Busca un correo
-        if( !usuario ){ //Y si existe entonces mandamos el error de que ya existe ese usuario
+        if (!usuario) { //Y si existe entonces mandamos el error de que ya existe ese usuario
             return res.status(400).json({
                 msg: 'Usuario / Password no son correctos - correo'
             })
         }
 
         // Si el usuario esta activo en mi DB
-        if( !usuario.estado ){ //Y si existe entonces mandamos el error de que ya existe ese usuario
+        if (!usuario.estado) { //Y si existe entonces mandamos el error de que ya existe ese usuario
             return res.status(400).json({
                 msg: 'Usuario / Password no son correctos - estado: false'
             })
         }
 
         // Verificar la contraseña
-        const validPassword = bcryptjs.compareSync( password, usuario.password ) //Compara el password que le mandamos por el body con el usuario.password de la base de datos
-        if( !validPassword ){
+        const validPassword = bcryptjs.compareSync(password, usuario.password) //Compara el password que le mandamos por el body con el usuario.password de la base de datos
+        if (!validPassword) {
             return res.status(400).json({
                 msg: 'Usuario / Password no son correctos - password'
             })
         }
 
         //Generar JWT
-        const token = await generarJWT( usuario.id );
+        const token = await generarJWT(usuario.id);
 
         res.json({
             usuario,
@@ -48,6 +50,60 @@ const login = async (req, res = response) => {
     }
 
 }
+
+const googleSignIn = async (req, res = response) => {
+
+    const { id_token } = req.body;
+
+    try {
+
+        const { correo, nombre, img } = await googleVerify( id_token );
+
+        let usuario = await Usuario.findOne({ correo });
+
+        if( !usuario ){
+            //Tengo que crearlo
+
+            const data = {
+                nombre,
+                correo,
+                password: 'pruebapassword',
+                img,
+                google: true,
+                rol: DefaultTransporter //Le pone un rol por defecto
+            };
+
+            usuario = new Usuario( data );
+            await usuario.save();
+
+        }
+
+        // Si el usuario en base de datos
+        if( !usuario.estado ){
+            return res.status(401).json({
+                msg: 'Hable con el administrador, usuario bloqueado'
+            })
+        }
+
+        // generar el jwt
+        const token = await generarJWT( usuario.id );
+
+        res.json({
+            usuario,
+            token
+        })
+
+    } catch (error) {
+        res.status(400).json({
+            ok: false,
+            msg: 'El token no se pudo verificar'
+        })
+    }
+
+
+}
+
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
